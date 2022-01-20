f9480eb8f0dbf07882a237ee1b933dc8677f5b13
Stephen Xian <stevexian56@gmail.com>
2022-01-20 10:26:24 +0800

    drivers: usb: Import Xiaomi changes
    Helium-Studio: Remove debug bloats and update for kernel 5.4.
    
    Co-authored-by: Demon000 <demonsingur@gmail.com>
    Co-authored-by: Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>
    Signed-off-by: Stephen Sinn <stevexian56@gmail.com>
    Signed-off-by: Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>

--- drivers/usb/pd/usbpd.h
+++ drivers/usb/pd/usbpd.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2016-2018, 2020 The Linux Foundation. All rights reserved.
+ * Copyright (C) 2021 XiaoMi, Inc.
  */
 
 #ifndef _USBPD_H
@@ -53,6 +54,18 @@ struct pd_phy_params {
 	u8		frame_filter_val;
 };
 
+struct usbpd_pdo {
+	bool pps;
+	int type;
+	int max_volt_mv;
+	int min_volt_mv;
+	int curr_ma;
+	int pos;
+};
+
+int usbpd_get_pps_status(struct usbpd *pd, u32 *status);
+int usbpd_fetch_pdo(struct usbpd *pd, struct usbpd_pdo *pdos);
+
 struct pd_phy_ops {
 	int (*open)(struct pd_phy_params *params);
 	int (*signal)(enum pd_sig_type sig);
@@ -75,5 +88,42 @@ static inline struct usbpd *usbpd_create(struct device *parent,
 }
 static inline void usbpd_destroy(struct usbpd *pd) { }
 #endif
+
+enum uvdm_state {
+	USBPD_UVDM_DISCONNECT,
+	USBPD_UVDM_CHARGER_VERSION,
+	USBPD_UVDM_CHARGER_VOLTAGE,
+	USBPD_UVDM_CHARGER_TEMP,
+	USBPD_UVDM_SESSION_SEED,
+	USBPD_UVDM_AUTHENTICATION,
+	USBPD_UVDM_VERIFIED,
+	USBPD_UVDM_REMOVE_COMPENSATION,
+	USBPD_UVDM_CONNECT,
+	USBPD_UVDM_NAN_ACK,
+};
+
+#define USB_PD_MI_SVID			0x2717
+#define USBPD_UVDM_SS_LEN		4
+#define USBPD_UVDM_VERIFIED_LEN		1
+
+#define VDM_HDR(svid, cmd0, cmd1) \
+       (((svid) << 16) | (0 << 15) | ((cmd0) << 8) \
+       | (cmd1))
+#define UVDM_HDR_CMD(hdr)	((hdr) & 0xFF)
+
+#define USBPD_VDM_RANDOM_NUM		4
+#define USBPD_VDM_REQUEST		0x1
+#define USBPD_ACK			0x2
+
+struct usbpd_vdm_data {
+	int ta_version;
+	int ta_temp;
+	int ta_voltage;
+	unsigned long s_secert[USBPD_UVDM_SS_LEN];
+	unsigned long digest[USBPD_UVDM_SS_LEN];
+};
+
+#define USBPD_WEAK_PPS_POWER		18000000
+#define USBPD_WAKK_PPS_CURR_LIMIT	1500000
 
 #endif /* _USBPD_H */
